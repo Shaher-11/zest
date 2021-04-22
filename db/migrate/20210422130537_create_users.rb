c9d6af7092ac27fb259db8724281a1ec314c15aa
Shaher-11 <shahershamroukh@gmail.com>
2021-04-22 15:18:07 +0200

    Add the required feilds in the table

--- db/migrate/20210422130537_create_users.rb
+++ db/migrate/20210422130537_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :first_name
+      t.string :username, null: false
+      t.string :first_name, null: false
       t.string :last_name
-      t.string :email
-      t.boolean :is_public
+      t.string :email, null: false
+      t.boolean :is_public, null: false, default: true
 
       t.timestamps
     end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  first_name :string           not null
+#  is_public  :boolean          default(TRUE), not null
+#  last_name  :string
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
 FactoryBot.define do
   factory :user do
     username { "MyString" }
