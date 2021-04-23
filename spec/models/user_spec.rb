bf7109a20294c2fc6973fd5cc09ac7362e74486e
Shaher-11 <shahershamroukh@gmail.com>
2021-04-23 11:27:53 +0200

    Add spec for email uniqness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def create_a_user(email: "#{SecureRandom.hex(4)}@example.org")
+    User.create!(
+      first_name: "Adam",
+      email: email,
+      username: SecureRandom.hex(4),
+    )
+  end
+  describe "#valid?" do
+    it "is valid when email is unique" do
+      create_a_user
+      user = User.new
+      user.email = "adam@example.org"
+      expect(user.valid?).to be true
+    end
+  end
 end
