22736567dafe983838b148c530f2a9253b4459ff
Shaher-11 <shahershamroukh@gmail.com>
2021-04-25 15:44:07 +0200

    Add seeders for the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :username, uniqueness: true
-  validates :field_name, uniqueness: true
   validates :first_name, presence: true
   validates :username, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
@@ -37,7 +36,7 @@ class User < ApplicationRecord
   has_many :followers,-> { where("bonds.state = ?", Bond::FOLLOWING) },through: :inward_bonds,source: :user
 
   before_save :ensure_proper_name_case
-  
+
   private
     def ensure_proper_name_case
       self.first_name = first_name.capitalize
