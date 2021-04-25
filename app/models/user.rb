25a4447fdbad42a5d37f09b33c2245b416ff53ae
Shaher-11 <shahershamroukh@gmail.com>
2021-04-25 15:04:56 +0200

    Associate users through bonds

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,16 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :username, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_many :posts
+  has_many :bonds 
+  has_many :followings,
+  -> { where("bonds.state = ?", Bond::FOLLOWING) },
+  through: :bonds,
+  source: :friend
+  has_many :follow_requests,
+  -> { where("bonds.state = ?", Bond::REQUESTING) },
+  through: :bonds,
+  source: :friend
+  has_many :inward_bonds,class_name: "Bond",foreign_key: :friend_id
+  has_many :followers,-> { where("bonds.state = ?", Bond::FOLLOWING) },through: :inward_bonds,source: :user
 end
