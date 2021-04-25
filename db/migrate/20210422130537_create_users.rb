340e6cb19c4602de175c502c4b73f4914a96a094
Shaher-11 <shahershamroukh@gmail.com>
2021-04-25 18:12:23 +0200

    Add the devise vew and run migrateion

--- db/migrate/20210422130537_create_users.rb
+++ db/migrate/20210422130537_create_users.rb
@@ -4,10 +4,12 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :username, null: false
       t.string :first_name, null: false
       t.string :last_name
+      t.string :email, null: false
       t.boolean :is_public, null: false, default: true
 
       t.timestamps
     end
     add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
   end
 end
