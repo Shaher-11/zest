1529cadabdefba5a9f1846cd678d9986a78f2efa
Shaher-11 <shahershamroukh@gmail.com>
2021-04-26 16:58:47 +0200

    Add validations to accept username file din user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+          authentication_keys: [:login]
+          attr_writer :login
+  validates_uniqueness_of :email
   validates :email, uniqueness: true
   validates :username, uniqueness: true
   validates :first_name, presence: true
@@ -50,6 +53,20 @@ class User < ApplicationRecord
   source: :user
 
   before_save :ensure_proper_name_case
+  
+  def login
+    @login || username || email
+  end
+
+  def self.find_authenticatable(login)
+    where("username = :value OR email = :value", value: login).first
+  end
+
+  def self.find_for_database_authentication(conditions)
+    conditions = conditions.dup
+    login = conditions.delete(:login).downcase
+    find_authenticatable(login)
+  end
 
   private
     def ensure_proper_name_case
