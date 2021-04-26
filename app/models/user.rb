772a78dc7ab8f45cf4e7380feb8d44a1f5b96819
Shaher-11 <shahershamroukh@gmail.com>
2021-04-26 17:12:31 +0200

    refactor forget password page

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-          authentication_keys: [:login]
+          authentication_keys: [:login],
+          reset_password_keys: [:login]
           attr_writer :login
   validates_uniqueness_of :email
   validates :email, uniqueness: true
@@ -53,7 +54,7 @@ class User < ApplicationRecord
   source: :user
 
   before_save :ensure_proper_name_case
-  
+
   def login
     @login || username || email
   end
@@ -68,6 +69,24 @@ class User < ApplicationRecord
     find_authenticatable(login)
   end
 
+  def self.send_reset_password_instructions(conditions)
+    recoverable = find_recoverable_or_init_with_errors(conditions)
+    if recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
+  def self.find_recoverable_or_init_with_errors(conditions)
+    conditions = conditions.dup
+    login = conditions.delete(:login).downcase
+    recoverable = find_authenticatable(login)
+    unless recoverable
+      recoverable = new(login: login)
+      recoverable.errors.add(:login, login.present? ? :not_found : :blank)
+    end
+    recoverable
+  end
+  
   private
     def ensure_proper_name_case
       self.first_name = first_name.capitalize
