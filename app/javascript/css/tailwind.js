f6561cf8f5556ca699d974b3e6cd369bd7352fea
Shaher-11 <shahershamroukh@gmail.com>
2021-04-26 16:46:04 +0200

    Add signup styling

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -5,7 +5,13 @@ module.exports = {
         '72': '18rem',
         '84': '21rem',
         '96': '24rem',
-      }
+      },
+      colors: {
+        main: {
+          default: "#60b0e2",
+          "500": "#41a1dd",
+        }
+      },
     },
   },
   variants: {},
