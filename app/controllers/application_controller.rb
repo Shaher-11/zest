a9eac255fc273449af89e871872b742752f2d3f7
Shaher-11 <shahershamroukh@gmail.com>
2021-04-27 17:51:19 +0200

    Style timeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,22 @@ class ApplicationController < ActionController::Base
 
   private
   
-    def layout_by_resource
-      devise_controller? ? "session" : "application"
+  def member_controller?
+    return false if controller_path == "home"
+    true
+  end
+
+  def layout_by_resource
+    case
+    when devise_controller? then "session"
+    when member_controller? then "member"
+    else "application"
     end
+  end
+  
+  def layout_by_resource
+    devise_controller? ? "session" : "application"
+  end
 
     protected
 
