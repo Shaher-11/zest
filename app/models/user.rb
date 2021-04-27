2f73491f6490f25f74e30f1a1017a4cf94c0ebe0
Shaher-11 <shahershamroukh@gmail.com>
2021-04-27 17:37:34 +0200

    Add timeline helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,10 @@ class User < ApplicationRecord
 
   before_save :ensure_proper_name_case
 
+  def to_param
+    username
+  end
+  
   def login
     @login || username || email
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   authenticate :user do
     resources :timelines,
       only: [:index, :show]
+      param: :username
   end
 end
