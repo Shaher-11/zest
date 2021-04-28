c7bafb3f58df8fda883688b28baf5005bbc7081f
Shaher-11 <shahershamroukh@gmail.com>
2021-04-28 17:16:45 +0200

    Add Post controller

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,14 +1,6 @@
 module.exports = {
   theme: {
     extend: {
-      spacing: {
-        '14': '3.5rem',
-        '72': '18rem',
-        '84': '21rem',
-        '96': '24rem',
-        '168': '42rem',
-        '192': '48rem',
-      },
       colors: {
         main: {
           default: "#4C51BF",
@@ -18,11 +10,20 @@ module.exports = {
         },
         dark: {
           "400": "#8a8a8a",
+          "500": "#4f4f4f",
           "600": "#343538",
           "700": "#2a2b2d",
           "800": "#18191a"
         }
       },
+      spacing: {
+        '14': '3.5rem',
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '168': '42rem',
+        '192': '48rem',
+      }
     },
   },
   variants: {},
